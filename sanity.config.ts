1c71ad5db9307666120cee032775a4b5809e270b
Hamish Burke <hamish.burke@alphero.com>
2026-03-09 20:39:37 +1300

    Update sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,34 +1,34 @@
-import { defineConfig } from 'sanity'
-import { structureTool } from 'sanity/structure'
-import { visionTool } from '@sanity/vision'
-import { schemaTypes } from './sanity/index'
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./sanity/index";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 
 export default defineConfig({
-  name: 'cole-website',
-  title: 'Cole Anderson — CMS',
-  basePath: '/studio',
+  name: "cole-website",
+  title: "Cole Anderson — CMS",
+  basePath: "/admin/cms",
   projectId,
   dataset,
   plugins: [
     structureTool({
       structure: (S) =>
         S.list()
-          .title('Content')
+          .title("Content")
           .items([
             S.listItem()
-              .title('Projects')
-              .child(S.documentTypeList('project').title('Projects')),
+              .title("Projects")
+              .child(S.documentTypeList("project").title("Projects")),
             S.divider(),
             S.listItem()
-              .title('Site Settings')
+              .title("Site Settings")
               .child(
                 S.document()
-                  .schemaType('siteSettings')
-                  .documentId('siteSettings')
-                  .title('Site Settings'),
+                  .schemaType("siteSettings")
+                  .documentId("siteSettings")
+                  .title("Site Settings"),
               ),
           ]),
     }),
@@ -37,4 +37,4 @@ export default defineConfig({
   schema: {
     types: schemaTypes,
   },
-})
+});
